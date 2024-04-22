aedc0bb2b54839e486f9ac30e7462a54c5714907
fin_walkes <158414030+finwalkes@users.noreply.github.com>
2024-04-22 08:32:48 +0700

    Add files via upload

---
+++ iku/pages/5FAQ.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+st.title("FAQ & Pendapat Ahli")
+st.write("---")
+
+# Informasi Kualitas Udara
+st.header("Apa itu Kualitas Udara?")
+st.write("Kualitas udara merujuk pada kandungan berbagai polutan dalam udara yang dapat mempengaruhi kesehatan manusia, lingkungan, dan ekosistem. Polutan udara umum meliputi partikulat, gas beracun, dan zat kimia lainnya.")
+
+# Cara Perhitungan Kualitas Udara di Indonesia
+st.header("Bagaimana Cara Perhitungan Kualitas Udara di Indonesia?")
+st.write("Di Indonesia, kualitas udara dihitung berdasarkan standar yang ditetapkan oleh Kementerian Lingkungan Hidup dan Kehutanan (KLHK). Standar ini mencakup pengukuran berbagai parameter udara seperti PM10, SO2, NO2, CO, dan O3.")
+
+# Pemantauan Udara di Lampung
+st.header("Bagaimana Pemantauan Udara di Lampung Saat Ini?")
+st.write("Pemantauan udara di Lampung saat ini dilakukan melalui beberapa pos pemantau udara yang tersebar di berbagai lokasi strategis di provinsi Lampung. Data pemantauan ini biasanya tersedia secara online dan dapat diakses oleh masyarakat.")
+
+# Perkiraan Kualitas Udara di Masa Depan
+st.header("Bagaimana Cara Memperkirakan Kualitas Udara di Masa Depan?")
+st.write("Perkiraan kualitas udara di masa depan dapat dilakukan menggunakan model prediksi berdasarkan faktor-faktor seperti pola lalu lintas, aktivitas industri, cuaca, dan kebijakan lingkungan.")
+
+# FAQ
+st.header("Pendapat Ahli")
+st.subheader("Apakah di Lampung sudah memiliki sistem prediksi kualitas udara?")
+st.write("Jawaban pertanyaan 1.")
+st.subheader("Apakah di Lampung memiliki aplikasi pemantauan kualitas udara?")
+st.write("Jawaban pertanyaan 2.")
+st.subheader("Apakah ")
+st.write("Jawaban pertanyaan 3.")
+
+# Footer
+st.markdown("""
+    ---
+    Skripsi © 2024 Bagus Wahyu Pratomo. 2015061003
+    """)
+
+st.sidebar.markdown("""
+    © 2024 Bagus Wahyu Pratomo.
+    """)
