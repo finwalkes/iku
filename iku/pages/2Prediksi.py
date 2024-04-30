4b60885322407a8624c01c0c92011610c7adabd1
fin_walkes <158414030+finwalkes@users.noreply.github.com>
2024-04-30 17:14:44 +0700

    Update 2Prediksi.py

--- iku/pages/2Prediksi.py
+++ iku/pages/2Prediksi.py
@@ -5,6 +5,7 @@ import numpy as np
 from glom import glom
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score
 
 # Function to preprocess the input data
 def preprocess_input_data(input_data):
@@ -16,7 +17,7 @@ def preprocess_input_data(input_data):
 # Function to make prediction
 def predict_air_quality(features):
     # Load your pre-trained LSTM model
-    model = load_model('iku/model/model.h5')  # Load your LSTM model
+    model = load_model('model\\psw.h5')  # Load your LSTM model
 
     # Make prediction
     prediction = model.predict(features)
@@ -46,8 +47,12 @@ def load_and_process_data(lat, lon, api_key):
     df_city = df[["city_name", "lat", "lon"]]
     df_merge = pd.concat([df_city, df_aqi], axis=1, sort=False)
     df_merge['timestamp_local'] = pd.to_datetime(df_merge['timestamp_local'])
-    # Drop UTC column\
+    # df_merge.set_index('timestamp_local', inplace=True)
     return df_merge
+
+def calculate_r_squared(actual_values, predicted_values):
+    r_squared = r2_score(actual_values, predicted_values)
+    return r_squared
 
 # Streamlit UI
 st.header('Forcasting Kualitas Udara Menggunakan LSTM')
@@ -106,57 +111,69 @@ elif point == "Pesisir Barat":
 
 
 if st.button('Predict Air Quality'):
-    # Load and process data
     df_merge = load_and_process_data(lat, lon, api_key)
-    
+
     st.subheader("Forecasting Kualitas udara dalam 36 Jam")
-    
+
     # Extract features for prediction
     features_for_prediction = df_merge[['aqi', 'pm10', 'pm25', 'o3', 'so2', 'no2', 'co']].values
-    
+
     # Preprocess input data
     input_features_processed = preprocess_input_data(features_for_prediction)
-    
+
     # Reshape features for LSTM input shape
     features_reshaped = np.reshape(input_features_processed, (int(input_features_processed.shape[0] / 6), 6, input_features_processed.shape[1]))
-    
+
     # Make prediction
     prediction = predict_air_quality(features_reshaped)
-    
+
     # Fit objek StandardScaler dengan data historis
     scaler = StandardScaler()
     scaler.fit(df_merge[['aqi', 'pm10', 'pm25', 'o3', 'so2', 'no2', 'co']])
-    
+
     # Transformasi prediksi ke skala aslinya
-    predictions = np.reshape(prediction, (int(input_features_processed.shape[0] / 6)*3, 7))
+    predictions = np.reshape(prediction, (int(input_features_processed.shape[0] / 6)*4, 7))
     predictions = scaler.inverse_transform(predictions)
     predictions = pd.DataFrame(predictions, columns=['aqi_pred', 'pm10_pred', 'pm25_pred', 'o3_pred', 'so2_pred', 'no2_pred', 'co_pred'])
-    
-    # Buat timestamp untuk data prediksi
-    forecast_dates = pd.date_range(start=df_merge['timestamp_local'].iloc[-1], periods=predictions.shape[0] + 1, freq='H')[1:]
-    predictions['timestamp_local'] = forecast_dates
-    
-    # Gabungkan prediksi dengan data historis
-    df_final = pd.concat([df_merge.reset_index(drop=True), predictions], axis=0, ignore_index=True)
-    
-    # Tampilkan hasil prediksi
-    st.write("Data Historis")
-    st.write(df_merge)
-    
-    st.write("Data Prediksi")
-    st.write(predictions)
-    
-   # Plot real and predicted values for each feature separately
+
+    # Concatenate df_merge and predictions horizontally
+    df_final = pd.concat([df_merge.reset_index(drop=True), predictions], axis=1)
+    col1, col2 = st.columns(2)
+    with col1:
+        # Display historical data
+        st.write("Historical Data")
+        st.write(df_merge)
+
+    with col2:
+        # Display predicted data
+        st.write("Predicted Data")
+        st.write(predictions)
+
+    # Plot real and predicted values for each feature separately
     for feature in ['aqi', 'pm10', 'pm25', 'o3', 'so2', 'no2', 'co']:
-        st.subheader(f'Predicted {feature.capitalize()}')
-        chart_data = df_final[['timestamp_local', feature, f'{feature}_pred']]
+        st.subheader(f'Predicted vs Real {feature.capitalize()}')
+        # Ensure that the column names exist in df_final
+        if feature in df_final.columns and f'{feature}_pred' in df_final.columns:
+            chart_data = df_final[['timestamp_local', feature, f'{feature}_pred']].set_index('timestamp_local')
+            st.line_chart(chart_data)
+        else:
+            st.write(f"Column names for {feature} or {feature}_pred not found in df_final.")
+    
+    # Define actual_values and predicted_values
+    actual_values = df_final[['aqi', 'pm10', 'pm25', 'o3', 'so2', 'no2', 'co']]
+    predicted_values = df_final[['aqi_pred', 'pm10_pred', 'pm25_pred', 'o3_pred', 'so2_pred', 'no2_pred', 'co_pred']]
+
+    # # Handle NaN values in actual_values and predicted_values
+    # actual_values.fillna(actual_values.mean(), inplace=True)
+    # predicted_values.fillna(predicted_values.mean(), inplace=True)
+
+    # Calculate R-squared values
+    r_squared_values = [calculate_r_squared(actual_values[feature], predicted_values[f'{feature}_pred']) for feature in actual_values.columns]
+
+    st.subheader("R-squared Values")
+    for i, feature in enumerate(actual_values.columns):
+        st.write(f"{feature.capitalize()}: {r_squared_values[i]}")
         
-        # Plot the line chart
-        st.line_chart(chart_data.set_index('timestamp_local'))
-
-
-
-
 # Footer
     st.markdown("""
         ---
